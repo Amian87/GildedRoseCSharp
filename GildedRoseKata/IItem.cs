37caafaf57452d34626833db4b1a01c41e98a76d
Abdulla Mian <miana@claycorp.com>
2020-05-27 08:55:23 -0500

    created IItem interface

---
+++ GildedRoseKata/IItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GildedRoseKata
+{
+    public interface IItem
+    {
+        void UpdateItemQuality();
+
+        string Name { get; set; }
+        int Quality { get; set; } 
+        int SellIn { get; set; }
+    }
+}
--- GildedRoseKata/Item.cs
+++ GildedRoseKata/Item.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace GildedRoseKata
 {
-    public class Item
+    public class Item : IItem
     {
         public string Name { get; set; }
         public int SellIn { get; set; }
