19558b5ff6eaf75ba0e86235a48b473144033e40
Abdulla Mian <miana@claycorp.com>
2020-06-09 03:44:45 -0500

    concert ticket quality

--- GildedRoseKata.Tests/ConcertTicketValueTest.cs
+++ GildedRoseKata.Tests/ConcertTicketValueTest.cs
@@ -1,7 +1,54 @@
-﻿namespace GildedRoseKata.Tests
+﻿using NUnit.Framework;
+
+namespace GildedRoseKata.Tests
 {
     public class ConcertTicketValueTest
     {
+        [Test]
+        public void ConcertTicketValueIncreasesByOneWhenThereAreMoreThanTenDaysLeft()
+        {
+            ConcertTicketValue concertTicket = new ConcertTicketValue {Name = "foo", Quality = 10, SellIn = 11};
+            concertTicket.UpdateItemQuality();
+            Assert.AreEqual(11, concertTicket.Quality);
+
+        }
+
+        [Test]
+        public void ConcertTicketQualityIncreasesByTwoWhenThereAreTenDaysOrLess()
+        {
+            ConcertTicketValue concertTicket = new ConcertTicketValue { Name = "foo", Quality = 13, SellIn = 9 };
+            concertTicket.UpdateItemQuality();
+            Assert.AreEqual(15, concertTicket.Quality);
+
+        }
+
+        [Test]
+        public void ConcertTicketQualityIncreasesByThreeWhenThereAreFiveDaysOrLess()
+        {
+            ConcertTicketValue concertTicket = new ConcertTicketValue { Name = "foo", Quality = 15, SellIn = 4 };
+            concertTicket.UpdateItemQuality();
+            Assert.AreEqual(18, concertTicket.Quality);
+
+        }
+
+
+        [Test]
+        public void ConcertTicketQualityFallsToZeroAfterSellIn()
+        {
+            ConcertTicketValue concertTicket = new ConcertTicketValue { Name = "foo", Quality = 15, SellIn = -1 };
+            concertTicket.UpdateItemQuality();
+            Assert.AreEqual(0, concertTicket.Quality);
+
+        }
+
+        [Test]
+        public void ConcertTicketQualityIsNeverGreaterThanFifty()
+        {
+            ConcertTicketValue concertTicket = new ConcertTicketValue { Name = "foo", Quality = 15, SellIn = -1 };
+            concertTicket.UpdateItemQuality();
+            Assert.AreEqual(0, concertTicket.Quality);
+
+        }
 
     }
 }
--- GildedRoseKata/ConcertTicketValue.cs
+++ GildedRoseKata/ConcertTicketValue.cs
@@ -4,7 +4,28 @@
     {
         public void UpdateItemQuality()
         {
-            throw new System.NotImplementedException();
+            Quality += 1;
+
+            if (SellIn < 5)
+            {
+                Quality += 1;
+            }
+
+            if (SellIn < 10)
+            {
+                Quality += 1;
+            }
+
+            if (Quality > 50)
+            {
+                Quality = 50;
+            }
+
+            if (SellIn < 0)
+            {
+                Quality = 0;
+            }
+   
         }
 
         public string Name { get; set; }
