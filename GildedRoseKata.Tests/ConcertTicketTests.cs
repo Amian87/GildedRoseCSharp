4a9ad1928feeca14f1768b68bc82ffc3032c3322
Abdulla Mian <miana@claycorp.com>
2020-06-21 01:40:19 -0500

    Create ConcertTicket and QualityDepreciates

---
+++ GildedRoseKata.Tests/ConcertTicketTests.cs
@@ -0,0 +1,44 @@
+﻿using NUnit.Framework;
+
+namespace GildedRoseKata.Tests
+{
+    public class ConcertTicketTests
+    {
+        [Test]
+        public void ConcertPassQualityIncreasesByTwoWhenThereAreTenDaysLeft()
+        {
+            ConcertTicket concertTicket = new ConcertTicket {Name = "foo", Quality = 90, SellIn = 10};
+            concertTicket.UpdateQuality();
+            Assert.AreEqual(92, concertTicket.Quality);
+            Assert.AreEqual(9, concertTicket.SellIn);
+        }
+
+        [Test]
+        public void ConcertPassQualityIncreasesByThreeWhenThereAreFiveDaysLeft()
+        {
+            ConcertTicket concertTicket = new ConcertTicket {Name = "foo", Quality = 97, SellIn = 5};
+            concertTicket.UpdateQuality();
+            Assert.AreEqual(100, concertTicket.Quality);
+            Assert.AreEqual(4, concertTicket.SellIn);
+        }
+
+        [Test]
+        public void ConcertPassQualityIncreasesByOneWhenThereAreMoreThanTenDaysLeft()
+        {
+            ConcertTicket concertTicket = new ConcertTicket {Name = "foo", Quality = 60, SellIn = 11};
+            concertTicket.UpdateQuality();
+            Assert.AreEqual(61, concertTicket.Quality);
+            Assert.AreEqual(10, concertTicket.SellIn);
+        }
+
+        [Test]
+        public void ConcertPassQualityDropsToZeroAfterConcert()
+        {
+            ConcertTicket concertTicket = new ConcertTicket {Name = "foo", Quality = 60, SellIn = 0};
+            concertTicket.UpdateQuality();
+            Assert.AreEqual(0, concertTicket.Quality);
+            Assert.AreEqual(-1, concertTicket.SellIn);
+        }
+
+    }
+}
---
+++ GildedRoseKata.Tests/QualityDepreciatesTest.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+
+namespace GildedRoseKata.Tests
+{
+    public class QualityDepreciatesTest
+    {
+        [Test]
+        public void QualityOfAnItemIsNeverNegative()
+        {
+            QualityDepreciates qualityDepreciates = new QualityDepreciates {Name = "foo", Quality = 0, SellIn = 5};
+            qualityDepreciates.UpdateQuality();
+            Assert.AreEqual(0, qualityDepreciates.Quality);
+            Assert.AreEqual(4, qualityDepreciates.SellIn);
+        }
+
+        [Test]
+        public void QualityDegradesByTwoAfterSellInDate()
+        {
+            QualityDepreciates qualityDepreciates = new QualityDepreciates {Name = "foo", Quality = 10, SellIn = 0};
+            qualityDepreciates.UpdateQuality();
+            Assert.AreEqual(8,qualityDepreciates.Quality);
+            Assert.AreEqual(-1, qualityDepreciates.SellIn);
+        }
+
+        [Test]
+        public void QualityDegradesByOneBeforeSellInDate()
+        {
+            QualityDepreciates qualityDepreciates = new QualityDepreciates {Name = "foo", Quality = 15, SellIn = 4};
+            qualityDepreciates.UpdateQuality();
+            Assert.AreEqual(14, qualityDepreciates.Quality);
+            Assert.AreEqual(3, qualityDepreciates.SellIn);
+        }
+    }
+}
