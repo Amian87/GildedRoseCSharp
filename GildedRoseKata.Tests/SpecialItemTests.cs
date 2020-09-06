6297d912bb0755c939e76170a4526bb3685ec53e
Abdulla Mian <miana@claycorp.com>
2020-09-06 05:43:26 -0500

    create specialItem

---
+++ GildedRoseKata.Tests/SpecialItemTests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+
+namespace GildedRoseKata.Tests
+{
+    public class SpecialItemTests
+    {
+        [Test]
+        public void QualityDegradesTwiceAsNormal()
+        {
+            SpecialItem specialItem = new SpecialItem {Name = "foo", Quality = 30, SellIn = 10};
+            specialItem.UpdateQuality();
+            Assert.AreEqual(28, specialItem.Quality);
+            Assert.AreEqual(9, specialItem.SellIn);
+        }
+
+        [Test]
+        public void QualityIsNeverNegative()
+        {
+            SpecialItem specialItem = new SpecialItem { Name = "foo", Quality = 0, SellIn = 10 };
+            specialItem.UpdateQuality();
+            Assert.AreEqual(0, specialItem.Quality);
+            Assert.AreEqual(9, specialItem.SellIn);
+        }
+    }
+}
