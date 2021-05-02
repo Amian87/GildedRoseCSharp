fe3f38d59b4d2943ac7c969d72b3152cdf66e4b2
Abdulla Mian <miana@claycorp.com>
2021-05-02 08:13:17 -0500

    deleted Item class

--- GildedRoseKata.Tests/GildedRoseTests.cs
+++ GildedRoseKata.Tests/GildedRoseTests.cs
@@ -9,7 +9,7 @@ namespace GildedRoseKata.Tests
         [Test]
         public void TestQualityDegradesTwiceAsFastAfterSellInDate()
         {
-            List<Item> item = new List<Item> {new Item(){Name = "foo", Quality = 10, SellIn = 0}};
+            List<IItem> item = new List<IItem> {new QualityDepreciates(){Name = "foo", Quality = 10, SellIn = 0}};
             GildedRose app = new GildedRose(item);
             app.UpdateQuality();
             Assert.AreEqual(8, item[0].Quality);
@@ -19,7 +19,7 @@ namespace GildedRoseKata.Tests
         [Test]
         public void TestQualityOfAnItemIsNeverNegative()
         {
-            List<Item> item = new List<Item>() {new Item() {Name = "foo", Quality = 0, SellIn = 5}};
+            List<IItem> item = new List<IItem>() {new QualityDepreciates() {Name = "foo", Quality = 0, SellIn = 5}};
             GildedRose app = new GildedRose(item);
             app.UpdateQuality();
             Assert.AreEqual(0, item[0].Quality);
@@ -29,7 +29,7 @@ namespace GildedRoseKata.Tests
         [Test]
         public void TestAgedBrieQualityIncreasesByOneBeforeSellInDate()
         {
-            List<Item> item = new List<Item>() {new Item{Name = "Aged Brie", Quality = 15, SellIn = 10}};
+            List<IItem> item = new List<IItem>() {new QualityAppreciates(){Name = "Aged Brie", Quality = 15, SellIn = 10}};
             GildedRose app = new GildedRose(item);
             app.UpdateQuality();
             Assert.AreEqual(16, item[0].Quality);
@@ -39,7 +39,7 @@ namespace GildedRoseKata.Tests
         [Test]
         public void TestQualityIsNeverGreaterThanFifty()
         {
-            List<Item> item = new List<Item>() {new Item(){Name = "Aged Brie", Quality = 50, SellIn = 13}};
+            List<IItem> item = new List<IItem>() {new QualityAppreciates(){Name = "Aged Brie", Quality = 50, SellIn = 13}};
             GildedRose app = new GildedRose(item);
             app.UpdateQuality();
             Assert.AreEqual(50, item[0].Quality);
@@ -49,7 +49,7 @@ namespace GildedRoseKata.Tests
         [Test]
         public void TestAgedBrieQualityIncreasesByTwoAfterSellInDate()
         {
-            List<Item> item = new List<Item>() {new Item() {Name = "Aged Brie", Quality = 40, SellIn = 0}};
+            List<IItem> item = new List<IItem>() {new QualityAppreciates() {Name = "Aged Brie", Quality = 40, SellIn = 0}};
             GildedRose app = new GildedRose(item);
             app.UpdateQuality();
             Assert.AreEqual(42, item[0].Quality);
@@ -59,7 +59,7 @@ namespace GildedRoseKata.Tests
         [Test]
         public void TestSulfurasQualityAndSellInRemainsTheSame()
         {
-            List<Item> item = new List<Item>() {new Item() {Name = "Sulfuras, Hand of Ragnaros", Quality = 10, SellIn = 10}};
+            List<IItem> item = new List<IItem>() {new FixedQuality() {Name = "Sulfuras, Hand of Ragnaros", Quality = 10, SellIn = 10}};
             GildedRose app = new GildedRose(item);
             app.UpdateQuality();
             Assert.AreEqual(10, item[0].Quality);
@@ -69,7 +69,7 @@ namespace GildedRoseKata.Tests
         [Test]
         public void TestBackstagePassQualityIncreasesByOneBeforeTenDays()
         {
-            List<Item> item = new List<Item>() {new Item() {Name = "Backstage passes to a TAFKAL80ETC concert", Quality = 40, SellIn = 11} };
+            List<IItem> item = new List<IItem>() {new ConcertTicketQuality() {Name = "Backstage passes to a TAFKAL80ETC concert", Quality = 40, SellIn = 11} };
             GildedRose app = new GildedRose(item);
             app.UpdateQuality();
             Assert.AreEqual(41, item[0].Quality);
@@ -79,7 +79,7 @@ namespace GildedRoseKata.Tests
         [Test]
         public void TestBackstagePassQualityIncreasesByTwoBeforeFiveDays()
         {
-            List<Item> item = new List<Item>() {new Item() {Name = "Backstage passes to a TAFKAL80ETC concert" , Quality = 35, SellIn = 10} };
+            List<IItem> item = new List<IItem>() {new ConcertTicketQuality() {Name = "Backstage passes to a TAFKAL80ETC concert" , Quality = 35, SellIn = 10} };
             GildedRose app = new GildedRose(item);
             app.UpdateQuality();
             Assert.AreEqual(37, item[0].Quality);
@@ -89,7 +89,7 @@ namespace GildedRoseKata.Tests
         [Test]
         public void TestBackStagePassQualityIncreasesByThreeLessThanFiveDays()
         {
-            List<Item> item = new List<Item>() {new Item() {Name = "Backstage passes to a TAFKAL80ETC concert", Quality = 37, SellIn = 5} };
+            List<IItem> item = new List<IItem>() {new ConcertTicketQuality() {Name = "Backstage passes to a TAFKAL80ETC concert", Quality = 37, SellIn = 5} };
             GildedRose app = new GildedRose(item);
             app.UpdateQuality();
             Assert.AreEqual(40, item[0].Quality);
@@ -99,7 +99,7 @@ namespace GildedRoseKata.Tests
         [Test]
         public void TestBackstagePassQualityIsZeroAfterSellInDate()
         {
-            List<Item> item = new List<Item>() {new Item() {Name = "Backstage passes to a TAFKAL80ETC concert", Quality = 50, SellIn = 0} };
+            List<IItem> item = new List<IItem>() {new ConcertTicketQuality() {Name = "Backstage passes to a TAFKAL80ETC concert", Quality = 50, SellIn = 0} };
             GildedRose app = new GildedRose(item);
             app.UpdateQuality();
             Assert.AreEqual(0, item[0].Quality);
